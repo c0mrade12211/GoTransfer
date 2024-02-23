886397e6aa97a644a130feea5da5b6d54eba270e
Daniil Stepanov <132468035+c0mrade12211@users.noreply.github.com>
2024-02-23 15:24:51 +0300

    Update tcp-simple-server.go
    Delete os.exit(1)
    Now we're can to get a lot of files

--- filetransfer/server/tcp-simple-server.go
+++ filetransfer/server/tcp-simple-server.go
@@ -11,31 +11,34 @@ func TCPserver(port int) {
 	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		fmt.Println("Error listening:", err.Error())
-		os.Exit(1)
+		return
 	}
 	defer ln.Close()
-
 	fmt.Printf("Server started. Listening on port %d...\n", port)
 
-	conn, err := ln.Accept()
-	if err != nil {
-		fmt.Println("Error accepting: ", err.Error())
-		os.Exit(1)
-	}
-	defer conn.Close()
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			continue
+		}
+		fmt.Println("Client connected.")
 
-	fmt.Println("Client connected.")
+		file, err := os.Create("received_file")
+		if err != nil {
+			fmt.Println("Error creating file: ", err.Error())
+			conn.Close()
+			continue
+		}
 
-	file, err := os.Create("received_file")
-	if err != nil {
-		fmt.Println("Error creating file: ", err.Error())
-		os.Exit(1)
-	}
-	defer file.Close()
+		_, err = io.Copy(file, conn)
+		if err != nil {
+			fmt.Println("Error copying file: ", err.Error())
+		} else {
+			fmt.Println("File received and saved.")
+		}
 
-	_, err = io.Copy(file, conn)
-	if err != nil {
-		fmt.Println("Error copying file: ", err.Error())
+		file.Close()
+		conn.Close()
 	}
-	fmt.Println("File received and saved.")
 }
